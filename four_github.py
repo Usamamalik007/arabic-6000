b9fb09933ff652227709756180d188ee5d360efc
Usamamalik007 <Usamamalik21@gmail.com>
2018-12-03 02:49:48 +0500

    Update four_github.py

--- four_github.py
+++ four_github.py
@@ -16,21 +16,21 @@ while aa<1000:
     y=randint(1, 3022)
     z=randint(1, 3022)
     print(str(w)+","+str(x)+","+str(y)+","+str(z)+",")
-    img1 = cv2.imread("arabic/6000"+str(x)+'.jpg')
-    img2 = cv2.imread("arabic/6000"+str(y)+'.jpg')
-    img3 = cv2.imread("arabic/6000"+str(z)+'.jpg')
-    img4 = cv2.imread("arabic/6000"+str(w)+'.jpg')	
+    img1 = cv2.imread("arabic-6000/"+str(x)+'.jpg')
+    img2 = cv2.imread("arabic-6000/"+str(y)+'.jpg')
+    img3 = cv2.imread("arabic-6000/"+str(z)+'.jpg')
+    img4 = cv2.imread("arabic-6000/"+str(w)+'.jpg')	
                
     vis = np.concatenate((img1, img2,img3,img4), axis=1)
-    cv2.imwrite("arabic/6000"+"_"+str(x)+"_"+str(y)+"_"+str(z)+"_"+str(w)+'.jpg', vis)
-    with open("arabic/6000"+"_"+str(x)+"_"+str(y)+"_"+str(z)+"_"+str(w)+'.gt.txt', 'w',encoding="utf-8") as f:
-        with open("arabic/6000"+str(x)+'.gt.txt', 'r',encoding="utf-8") as myfile:
+    cv2.imwrite("arabic-6000/"+"_"+str(x)+"_"+str(y)+"_"+str(z)+"_"+str(w)+'.jpg', vis)
+    with open("arabic-6000/"+"_"+str(x)+"_"+str(y)+"_"+str(z)+"_"+str(w)+'.gt.txt', 'w',encoding="utf-8") as f:
+        with open("arabic-6000/"+str(x)+'.gt.txt', 'r',encoding="utf-8") as myfile:
             a=myfile.read()
-        with open("arabic/6000"+str(y)+'.gt.txt', 'r',encoding="utf-8") as myfile:
+        with open("arabic-6000/"+str(y)+'.gt.txt', 'r',encoding="utf-8") as myfile:
             b=myfile.read()
-        with open("arabic/6000"+str(z)+'.gt.txt', 'r',encoding="utf-8") as myfile:
+        with open("arabic-6000/"+str(z)+'.gt.txt', 'r',encoding="utf-8") as myfile:
             c=myfile.read()
-        with open("arabic/6000"+str(w)+'.gt.txt', 'r',encoding="utf-8") as myfile:
+        with open("arabic-6000/"+str(w)+'.gt.txt', 'r',encoding="utf-8") as myfile:
             d=myfile.read()
 
         f.write(str(d) + " "+str(c)+" "+str(b)+" "+str(a) )
